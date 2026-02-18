14c345252d07868ee6b2ac9e1b949bff080402e6
El-Gabo-45 <gallardoguevarag@gmail.com>
2026-02-17 20:22:37 -0400

    Function random color added

---
+++ input-methods/dbclick/main.js
@@ -0,0 +1,7 @@
+import { randomColor } from '../modules/randomColor.js';
+
+const targetElement = document.querySelector('.block');
+const randomBtn = document.querySelector('.random-color');
+const resetBtn = document.querySelector('.reset-btn');
+
+randomColor(targetElement, randomBtn, resetBtn);
---
+++ input-methods/modules/randomColor.js
@@ -0,0 +1,15 @@
+export function randomColor(targetElement, randomBtn, resetBtn) {
+    if (!targetElement || !randomBtn) return;
+
+    randomBtn.addEventListener('dblclick', () => {
+        const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+        targetElement.style.setProperty('--custom-color', randomColor);
+        targetElement.classList.add('custom');
+    })
+
+    if (resetBtn) {
+        resetBtn.addEventListener('click', () => {
+            targetElement.classList.remove('custom');
+        })
+    }
+}
